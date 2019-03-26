719ef651ee38438931b7946fea1613329b4fba02
Omodox <khoroshcho@gmail.com>
2019-03-26 16:16:31 +0200

    start

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = 3000;
+const ip = '127.0.0.1';
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World');
+}).listen(port, ip);
+
+console.log(`server is running on ${ip}:${port}`);
