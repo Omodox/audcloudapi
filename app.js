6fafa31954a5b9e7c131230a6caecaeb724f5875
Omodox <khoroshcho@gmail.com>
2019-03-26 19:09:19 +0200

    test

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ http.createServer(function(request,response){
      
     response.end("Hello NodeJS!");
      
-}).listen(3000, "127.0.0.1",function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
+}).listen(80, "127.0.0.1",function(){
+    console.log("Сервер начал прослушивание запросов на порту 80");
 });
