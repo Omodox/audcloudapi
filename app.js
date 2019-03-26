07d792b3e27e55ec70781e712d343158f7788b2e
Omodox <khoroshcho@gmail.com>
2019-03-26 21:04:46 +0200

    express

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const http = require("http");
-http.createServer(function(request,response){
+// const http = require("http");
+// http.createServer(function(request,response){
      
-    response.end("Hello NodeJS!");
+//     response.end("Hello NodeJS!");
      
-}).listen(80, "185.143.145.35",function(){
-    console.log("Сервер начал прослушивание запросов на порту 80");
-});
+// }).listen(80, "185.143.145.35",function(){
+//     console.log("Сервер начал прослушивание запросов на порту 80");
+// });
+
+
+var express = require(express);
+
+var app = express();
+app.get('/',function(req,res){
+    res.send('hello world');
+})
+
+app.lisent(80,function() {
+    console.log('ok');
+})
