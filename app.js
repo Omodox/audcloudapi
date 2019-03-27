0126c5860612f4f55e37af9964f37c9a39d075e9
Omodox <khoroshcho@gmail.com>
2019-03-27 16:01:41 +0200

    post

--- app.js
+++ app.js
@@ -9,12 +9,26 @@
 
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var mongoClient = require('mongodb').MongoClient;
+var db;
+
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/',function(req,res){
     res.send('hello world');
 })
 
+app.post('/performer', function (req, res) {
+    console.log(req.body);
+    res.send('post data');
+});
+
 app.listen(80, function() {
     console.log('ok');
 })
+
+// MongoClient.connect('mongodb://localhost:27017/audcloud')
