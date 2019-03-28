b617c9262091b48b78cb533d73388fdf949930f1
Denis Khoroshcho <khoroshcho@gmail.com>
2019-03-28 22:43:44 +0200

    push

--- app.js
+++ app.js
@@ -16,29 +16,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get('/',function(req,res){
-    res.send('hello world 2');
+    
+    res.send('Hello ' + req.query.name );
 })
 
 
 app.post('/traks', function (req, res) {
 
-    
-   var element = req.body;
-
+    console.log(req.body);
+    var element = req.body;
     
         var perfromer = {
-            zId : element.name,
+            zId : element.zId,
             perfromerName : element.perfromerName,
             trackName : element.trackName,
         }
+
         db.collection('traks').insert(perfromer);
         console.log(perfromer);
 
-
     
-          res.send('lorem');
+          res.send(req.body.perfromerName);
     
 });
+
+app.get('/traks', function (req, res) {
+
+  
+
+    db.collection('traks').find().toArray(function (err,docs) {
+      res.send(docs);
+      });
+
+  });    
+
 
 
 app.listen(80, function() {
@@ -50,6 +61,7 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'audcloud';
     MongoClient.connect(url, function(err, client) {
          db = client.db(dbName);
+         
         
       }); 
 
