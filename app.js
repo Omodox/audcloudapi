cfbbed7e85301119952fa2c010f6571fa1f6f031
Denis Khoroshcho <khoroshcho@gmail.com>
2019-03-31 20:22:39 +0300

    test

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
     // // to the API (e.g. in case you use sessions)
     // res.setHeader('Access-Control-Allow-Credentials', true);
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
