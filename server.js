118b85d2a687c94317ead415668cfde920d56257
Omodox <khoroshcho@gmail.com>
2019-04-02 18:41:26 +0300

    errors

--- server.js
+++ server.js
@@ -33,6 +33,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 const tracksRouter = require('./api/routes/tracks.js');
 app.use('/tracks', tracksRouter);
 
+app.use((req,res,next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error,req,res,next) => {
+    res.status(error.status || 500); 
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
 
 
 // Certificate
