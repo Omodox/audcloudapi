fa3028bb5d15a25f01d61a16e4756bdb6ce6c43e
Denis Khoroshcho <khoroshcho@gmail.com>
2019-04-03 23:07:33 +0300

    zId

--- api/routes/tracks.js
+++ api/routes/tracks.js
@@ -6,17 +6,17 @@ const bearerToken = require('express-bearer-token');
 
 const url = 'mongodb://localhost:27017';
 const dbName = 'audcloud';
-    MongoClient.connect(url, function(err, client) {
-         db = client.db(dbName);
-      }); 
+MongoClient.connect(url, function (err, client) {
+    db = client.db(dbName);
+});
 
 
 router.get('/', (req, res, next) => {
 
-    var perfid = req.query.performerid; 
-    db.collection('tracks').find({performerId:ObjectId(perfid)}).sort({"rating" : -1}).toArray(function (err,docs) {
+    var perfid = req.query.performerid;
+    db.collection('tracks').find({ performerId: ObjectId(perfid) }).sort({ "rating": -1 }).toArray(function (err, docs) {
         res.status(200).json(docs);
-      });
+    });
 
 
 });
@@ -30,83 +30,96 @@ router.post('/', function (req, res) {
 
 
 
-      element.performerName =  element.performerName.replace('&amp;amp;', '&');
-      element.trackName =  element.trackName.replace('&amp;amp;', '&');
-      element.trackName =  element.trackName.replace('(', '');
-      element.trackName =  element.trackName.replace(')', '');
-      element.trackName =  element.trackName.replace('-', ' ');
-      var TimeNow = Math.floor(new Date() / 1000);
-        var track = {
-            zId : element.zId,
-            performerName : element.performerName,
-            trackName : element.trackName,
-            performerId :null,
-            keys: null,
-            genres: null,
-            duration: 0,
-            auditions: 0,
-            errors: 0,
-            createdTime: TimeNow,
-            rating: 0,
-            auditionsTime: 0,
-            likes: 0,
-            dislikes: 0,
+    element.performerName = element.performerName.replace('&amp;amp;', '&');
+    element.trackName = element.trackName.replace('&amp;amp;', '&');
+    element.trackName = element.trackName.replace('(', '');
+    element.trackName = element.trackName.replace(')', '');
+    element.trackName = element.trackName.replace('-', ' ');
+    var TimeNow = Math.floor(new Date() / 1000);
+    var track = {
+        zId: element.zId,
+        performerName: element.performerName,
+        trackName: element.trackName,
+        performerId: null,
+        keys: null,
+        genres: null,
+        duration: 0,
+        auditions: 0,
+        errors: 0,
+        createdTime: TimeNow,
+        rating: 0,
+        auditionsTime: 0,
+        likes: 0,
+        dislikes: 0,
+
+    }
+
+    db.collection('performers').find({ performerName: element.performerName }).toArray(function (err, docs) {
+
+        var keys = element.performerName.split(' ');
+        var trackKeys = keys.concat(track.trackName.split(' '));
+        track.keys = trackKeys;
+        if (docs == '') {
+
+            var performer = {
+                "performerName": element.performerName,
+                "keys": keys,
+                "genres": null,
+
+            }
+            db.collection('performers').insertOne(performer, function (err, docsInserted) {
+
+                track.performerId = docsInserted.ops[0]._id;
+
+                createTrack();
+            });
 
         }
 
-        db.collection('performers').find({performerName:element.performerName}).toArray(function (err,docs) {
+        else {
 
-            var keys =  element.performerName.split(' ');
-            var trackKeys = keys.concat(track.trackName.split(' '));
-            track.keys = trackKeys;
+            track.performerId = docs[0]._id;
+            createTrack();
+        }
+
+
+    });
+
+    function createTrack() {
+        var searchParameter = {
+            $or: [
+                {
+                    $and:
+                        [
+                            { performerName: track.performerName },
+                            { trackName: track.trackName },
+
+                        ]
+                },
+                { zId: track.zId }
+            ]
+
+        };
+        db.collection('tracks').find(searchParameter).toArray(function (err, docs) {
             if (docs == '') {
-          
-                var performer =  {
-                    "performerName": element.performerName,
-                     "keys": keys,
-                     "genres": null,
-
-                     }
-                db.collection('performers').insertOne(performer, function(err,docsInserted){
-                   
-                    track.performerId = docsInserted.ops[0]._id;
-                   
-                    createTrack();
-                });
-
+                db.collection('tracks').insert(track);
             }
-
-            else {
-     
-                track.performerId = docs[0]._id;
-                createTrack();
-            }
+        });
+    }
 
 
-            });
+    res.send(req.body.perfromerName);
 
-            function createTrack() {
-                var searchParameter = { $and: [ { performerName: track.performerName }, { trackName: track.trackName } ]};
-                db.collection('tracks').find(searchParameter).toArray(function (err,docs) { 
-                    if (docs == '') {
-                        db.collection('tracks').insert(track);
-                    }
-                });
-            }
-
-    
-          res.send(req.body.perfromerName);
-    
 });
 
 
 router.delete('/:id', function (req, res) {
 
-    db.collection('users').find({sessions: req.token }).toArray(function (err,docs) {
+    db.collection('users').find({ sessions: req.token }).toArray(function (err, docs) {
 
         activeUser = docs[0];
         if (activeUser.role == "admin") {
-            db.collection('tracks').remove({_id:ObjectId(req.params.id)});
+            db.collection('tracks').remove({ _id: ObjectId(req.params.id) });
             res.status(200).json({
                 role: activeUser.role,
                 _id: req.params.id
@@ -116,10 +129,10 @@ router.delete('/:id', function (req, res) {
                 message: 'token is bad'
             })
         }
-       
-    });
-      
 
-  }); 
+    });
+
+
+});
 
 module.exports = router;
