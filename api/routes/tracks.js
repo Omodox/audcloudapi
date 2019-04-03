75641a8dad87f7a482637c24a6b81e8d0a5aaeea
Omodox <khoroshcho@gmail.com>
2019-04-03 10:50:58 +0300

    server tracks

--- api/routes/tracks.js
+++ api/routes/tracks.js
@@ -7,4 +7,80 @@ router.get('/', (req, res, next) => {
  });
 });
 
+app.post('/', function (req, res) {
+
+    console.log(req.body);
+    var element = req.body;
+
+
+
+      element.performerName =  element.performerName.replace('&amp;amp;', '&');
+      element.trackName =  element.trackName.replace('&amp;amp;', '&');
+      element.trackName =  element.trackName.replace('(', '');
+      element.trackName =  element.trackName.replace(')', '');
+      element.trackName =  element.trackName.replace('-', ' ');
+      var TimeNow = Math.floor(new Date() / 1000);
+        var track = {
+            zId : element.zId,
+            performerName : element.performerName,
+            trackName : element.trackName,
+            performerId :null,
+            keys: null,
+            genres: null,
+            duration: 0,
+            auditions: 0,
+            errors: 0,
+            createdTime: TimeNow,
+            rating: 0,
+            auditionsTime: 0,
+            likes: 0,
+            dislikes: 0,
+
+        }
+
+        db.collection('performers').find({performerName:element.performerName}).toArray(function (err,docs) {
+
+            var keys =  element.performerName.split(' ');
+            var trackKeys = keys.concat(track.trackName.split(' '));
+            track.keys = trackKeys;
+            if (docs == '') {
+          
+                var performer =  {
+                    "performerName": element.performerName,
+                     "keys": keys,
+                     "genres": null,
+
+                     }
+                db.collection('performers').insertOne(performer, function(err,docsInserted){
+                   
+                    track.performerId = docsInserted.ops[0]._id;
+                   
+                    createTrack();
+                });
+
+            }
+
+            else {
+     
+                track.performerId = docs[0]._id;
+                createTrack();
+            }
+
+
+            });
+
+            function createTrack() {
+                var searchParameter = { $and: [ { performerName: track.performerName }, { trackName: track.trackName } ]};
+                db.collection('tracks').find(searchParameter).toArray(function (err,docs) { 
+                    if (docs == '') {
+                        db.collection('tracks').insert(track);
+                    }
+                });
+            }
+
+    
+          res.send(req.body.perfromerName);
+    
+});
+
 module.exports = router;
