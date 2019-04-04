e318b684f4777ad041a2432ee9519d5607633abc
Denis Khoroshcho <khoroshcho@gmail.com>
2019-04-04 20:40:01 +0300

    likeTrack

---
+++ api/routes/likes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var crypto = require('crypto');
+const bearerToken = require('express-bearer-token');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'audcloud';
+    MongoClient.connect(url, function(err, client) {
+         db = client.db(dbName);
+      }); 
+
+// 
+
+
+router.post('/', function (req, res) {
+
+    var like = req.body;
+    var user_id = like._id;
+
+    db.collection('users').find({ sessions: req.token }).toArray(function (err, docs) {
+
+        if (docs.length > 0) {
+            activeUser = docs[0];
+            db.collection("users").update({ _id: ObjectId(user_id)},{$addToSet : {likes : {_id: like.track_id} }}, {multi:false});
+            res.status(200).json(
+                like
+            );
+    
+        }  else {
+            res.status(500).json({
+                message: 'token is bad'
+            })
+        }
+     
+
+
+    });
+
+});
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -49,6 +49,9 @@ app.use('/registration', registrationRouter);
 const loginRouter = require('./api/routes/login.js');
 app.use('/login', loginRouter);
 
+const likesRouter = require('./api/routes/likes.js');
+app.use('/likes', likesRouter);
+
 
 
 app.use((req,res,next) => {
