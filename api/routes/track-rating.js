7795701d8e823443f3a4d45f35dd057cbdeb4334
Omodox <khoroshcho@gmail.com>
2019-04-05 17:43:05 +0300

    fix

--- api/routes/track-rating.js
+++ api/routes/track-rating.js
@@ -35,7 +35,7 @@ router.post('/', function (req, res) {
                 activeUser = docs[0];
                 db.collection("users").update({ _id: ObjectId(activeUser._id)},{ $pull: { history: { _id : element._id } } } ); // remove from User list
                 db.collection("users").update({ _id: ObjectId(activeUser._id)},{$addToSet : {history : {_id: element._id} }}); // add from User list
-                var historyLength = activeUser.history.length();
+                var historyLength = activeUser.history.length;
                 if (historyLength > 10) {
                     db.collection("users").update( { _id: ObjectId(activeUser._id)}, { $pop: { history: 1 } } ); //Remove last of history user list
                 }
