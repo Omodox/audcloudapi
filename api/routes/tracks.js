1706455385c5881bb250fb7e905ae8467e9ba451
Omodox <khoroshcho@gmail.com>
2019-04-08 14:09:05 +0300

    fix

--- api/routes/tracks.js
+++ api/routes/tracks.js
@@ -20,11 +20,11 @@ router.get('/', (req, res, next) => {
         });
     }
     var rating = req.query.rating;
-    if (rating == true) {
+    if (rating == "true") {
         db.collection('tracks').find().sort({ "rating": -1 }).limit(100).toArray(function (err, docs) {
             res.status(200).json(docs);
         });
-    }
+    } 
 
 
  
