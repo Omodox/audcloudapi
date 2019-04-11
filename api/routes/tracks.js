640d142aba79bdcf19b7b5cc8c045526efa46ce2
Omodox <khoroshcho@gmail.com>
2019-04-11 18:33:41 +0300

    fix

--- api/routes/tracks.js
+++ api/routes/tracks.js
@@ -37,6 +37,12 @@ router.get('/', (req, res, next) => {
 router.get('/:id', (req, res, next) => {
 
     var trackId = req.params.id;
+
+    if (trackId.length < 24 || trackId.length > 24) {
+        res.status(200).json([]);
+        return true;
+    }
+
     db.collection('tracks').find({ _id: ObjectId(trackId) }).sort({ "rating": -1 }).toArray(function (err, docs) {
        
         if (docs.length > 0) {
